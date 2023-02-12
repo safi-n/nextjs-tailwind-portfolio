2f884e3a3466b63d98147bbbd6befd536edec4e6
Safi <Nurzai.safi@gmail.com>
2023-02-12 18:50:28 +0300

    feat: creative projects data file updated

--- data/creativeProjects.js
+++ data/creativeProjects.js
@@ -1,17 +1,64 @@
+import {v4 as uuid} from 'uuid'
+import logo1 from '../public/assets/imgs/MarkYourHome.jpg'
+import logo2 from '../public/assets/imgs/Meteros.jpg'
+import logo3 from '../public/assets/imgs/TetraClick-05.jpg'
 const creativeProjects = [
   {
-    id: '',
-    img: '',
-    title: ''
+    id: uuid(),
+    img: logo1,
+    title: '',
+    tags: ''
   },
   {
-    id: '',
-    img: '',
-    title: ''
+    id: uuid(),
+    img: logo2,
+    title: '',
+    tags: ''
   },
   {
-    id: '',
-    img: '',
-    title: ''
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
+  },
+  // posters area
+  {
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
+  },
+  {
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
+  },
+  {
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
+  },
+  // Brand ID
+  {
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
+  },
+  {
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
+  },
+  {
+    id: uuid(),
+    img: logo3,
+    title: '',
+    tags: ''
   },
 ]
+
+export default creativeProjects
