2844620706abc51625df9f70086e745e62a8bbee
Safi <Nurzai.safi@gmail.com>
2023-02-13 17:56:38 +0300

    feat: pop-up images for the photos section added

--- components/PhotosProjects.jsx
+++ components/PhotosProjects.jsx
@@ -1,15 +1,16 @@
-// import { useState } from "react";
+import { useState } from "react";
 import photosProjects from "@/data/photosProjects";
 import Image from "next/image";
 
 const PhotosProjects = () => {
-  // const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <>
       <ul className="creative-projects__lists">
         {photosProjects.map((project) => {
           return (
+            <>
               <li className="creative-projects__list--item" key={project.id}>
                 <Image
                   src={project.url}
@@ -17,23 +18,26 @@ const PhotosProjects = () => {
                   height="200"
                   alt={project.title}
                   priority
-                  // onClick={setSelectedImage(project)}
+                  onClick={() => setSelectedImage(project)}
                 />
               </li>
+            </>
           );
         })}
       </ul>
-      {/* {selectedImage && (
-        <div className="image__pop-up">
-          <Image
-            src={selectedImage.url}
-            width="200"
-            height="200"
-            alt={selectedImage.title}
-            priority
-          />
+      {selectedImage && (
+        <div className="photo-project__pop-up-container" onClick={() => setSelectedImage(null)}>
+          <div className="photo-project__pop-up--image-container">
+            <Image
+              src={selectedImage.url}
+              width="600"
+              height="600"
+              alt="pop-image"
+              priority
+            />
+          </div>
         </div>
-      )} */}
+      )}
     </>
   );
 };
