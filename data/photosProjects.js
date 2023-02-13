8d19423aa9be96406a66ddfa56d89db8a5c7f103
Safi <Nurzai.safi@gmail.com>
2023-02-13 19:02:31 +0300

    feat: new changes in pop up images modal

--- data/photosProjects.js
+++ data/photosProjects.js
@@ -1,6 +1,6 @@
 import {v4 as uuid} from 'uuid'
 import logo from '../public/assets/imgs/MarkYourHome.jpg'
-import logo1 from '../public/assets/imgs/Poster-digitalMarketing_ad.jpg'
+import logo1 from '../public/assets/imgs/Poster-RealEstate__MYH.jpg'
 const photosProject = [
   {
     id: uuid(),
