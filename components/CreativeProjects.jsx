24b388ce280544c6b0cffdbe8de8f896546a924b
Safi <Nurzai.safi@gmail.com>
2023-03-01 20:40:10 +0300

    feat: new changes in projects

--- components/CreativeProjects.jsx
+++ components/CreativeProjects.jsx
@@ -1,27 +1,55 @@
+import { useState } from "react";
 import creativeProjects from "@/data/creativeProjects";
 import Image from "next/image";
+import closeButton from "../public/assets/png/icon-closeButton.png";
 
 const CreativeProjects = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
-    <ul className="creative-projects__lists">
-              {creativeProjects.map((project) => {
-                return (
-                  <li
-                    className="creative-projects__list--item"
-                    key={project.id}
-                  >
-                    <Image
-                      src={project.img}
-                      width='auto'
-                      height='auto'
-                      alt={project.title}
-                      priority
-                      className="bg-contain"
-                    />
-                  </li>
-                );
-              })}
-            </ul>
+    <>
+      <ul className="creative-projects__lists">
+        {creativeProjects.map((project) => {
+          return (
+            <li className="creative-projects__list--item" key={project.id}>
+              <Image
+                src={project.img}
+                width="200"
+                height="200"
+                alt={project.title}
+                priority
+                onClick={() => setSelectedImage(project)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      {selectedImage && (
+        <div
+          className="photo-project__pop-up-container"
+          onClick={() => setSelectedImage(null)}
+        >
+          <div className="photo-project__pop-up-container--box">
+            <span className="photo-project__close-image-button">
+              <Image
+                src={closeButton}
+                alt="close-buttom"
+                width="auto"
+                height="auto"
+              />
+            </span>
+            <div className="photo-project__pop-up--image-container">
+              <Image
+                src={selectedImage.img}
+                width="auto"
+                height="auto"
+                alt="pop-image"
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
--- data/techProjects.js
+++ data/techProjects.js
@@ -18,7 +18,7 @@ const techProjects = [
     id: uuid(),
     logo: logo,
     company: 'Ras soul',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem pariatur rerum ad mollitia eius molestiae accusamus nam est natus magnam.'
+    description: "une maison d'édition numérique autodidacte et indépendante basée à Lyon, en France, qui cherche à fournir un contenu exclusif et de qualité à notre communauté de lecteurs dans le monde entier."
   },
   {
     id: uuid(),
