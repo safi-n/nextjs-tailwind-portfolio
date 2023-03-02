47e758bbe517102c002e601e65d63c7939404bca
Safi <Nurzai.safi@gmail.com>
2023-03-02 15:28:05 +0300

    feat: projects updated

--- data/techProjects.js
+++ data/techProjects.js
@@ -1,9 +1,18 @@
 import {v4 as uuid} from 'uuid'
+import logo_scriptsun from '../public/assets/png/logo_scriptsun.png'
 import logo_fintelion from '../public/assets/imgs/logo_fintelion.png'
 import logo_sbg from '../public/assets/imgs/SBGlogo.png'
+import logo_rassoul from '../public/assets/imgs/logo_rassoul.jpg'
 import logo from '../public/assets/png/JavascriptLogo.png'
 
 const techProjects = [
+  {
+    id: uuid(),
+    logo: logo_scriptsun,
+    company: 'ScriptSun',
+    description: "ScriptSun is a leading global vendor of computer software & mobile and hosting systems. ScriptSun's Largest projects are WoWonder, UltaHost Services, PlayTube, DeepSound, PixelPhoto and more.",
+    link: 'https://scriptsun.com/'
+  },
   {
     id: uuid(),
     logo: logo_fintelion,
@@ -15,11 +24,12 @@ const techProjects = [
     id: uuid(),
     logo: logo_sbg,
     company: 'SBGroup',
-    description: 'SBGroup has built a unique status among its valued clients. Our expert and capable staff provide professional services in consultancy, survey & design, procurement, logistics.'
+    description: 'SBGroup has built a unique status among its valued clients. Our expert and capable staff provide professional services in consultancy, survey & design, procurement, logistics.',
+    link: 'https://sadatbg.com/'
   },
   {
     id: uuid(),
-    logo: logo,
+    logo: logo_rassoul,
     company: 'Ras soul',
     description: "une maison d'édition numérique autodidacte et indépendante basée à Lyon, en France, qui cherche à fournir un contenu exclusif et de qualité à notre communauté de lecteurs dans le monde entier.",
     link: "https://rassoul.fr/accueil-2/"
