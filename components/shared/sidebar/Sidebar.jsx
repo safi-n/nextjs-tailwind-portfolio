116ce4408ccb252b5dacaa0233b5e26783b0af14
Safi <Nurzai.safi@gmail.com>
2023-03-16 20:00:24 +0300

    feat: changes in fonts

--- components/shared/sidebar/Sidebar.jsx
+++ components/shared/sidebar/Sidebar.jsx
@@ -2,27 +2,32 @@ import socialMediaIcons from "@/data/socialMediaIcons";
 import Image from "next/image";
 const Sidebar = () => {
   return (
-      <div className="slider-container">
-        <ul className="">
-          {socialMediaIcons.map((icon) => {
-            return (
-              <li key={icon.id} className="">
-                <div className="icon-side">
-                  <a href={icon.link} target="_blank" className="side-icon-link" rel="noreferrer">
-                    <Image
-                      src={icon.img}
-                      alt={icon.label}
-                      width="auto"
-                      height="auto"
-                      priority
-                    />
-                  </a>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="slider-container">
+      <ul>
+        {socialMediaIcons.map((icon) => {
+          return (
+            <li key={icon.id}>
+              <div className="icon-side">
+                <a
+                  href={icon.link}
+                  target="_blank"
+                  className="side-icon-link"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src={icon.img}
+                    alt={icon.label}
+                    width="auto"
+                    height="auto"
+                    priority
+                  />
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
