c2b831406fbffa2d9db6b211ba61deed08326c00
safi-n <nurzai.safi@gmail.com>
2023-04-02 22:37:49 +0300

    feat: new whatsapp icon added to the side line of the application

--- data/socialMediaIcons.js
+++ data/socialMediaIcons.js
@@ -3,6 +3,7 @@ import GhIcon from '../public/assets/svg/Icon awesome-github.svg'
 import BeIcon from '../public/assets/svg/Behance.svg'
 import LInIcon from '../public/assets/svg/Icon awesome-linkedin-in.svg'
 import UnSIcon from '../public/assets/svg/unsplashIcon.svg'
+import WTAPPIcon from '../public/assets/svg/whatsapp_business-icon.svg'
 const socialMediaIcons = [
   {
     id: uuid(),
@@ -27,6 +28,12 @@ const socialMediaIcons = [
     label: 'Un Splash',
     img: UnSIcon,
     link: 'https://unsplash.com/@safi_nurzai'
+  },
+  {
+    id: uuid(),
+    label: 'Whats-up',
+    img: WTAPPIcon,
+    link: 'https://wa.me/message/4JTT4GM6FXPZH1'
   },
 ];
 
